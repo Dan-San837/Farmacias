a6d32de08e1eb67f8524253a24f0c7bd063603ee
Dan-San837 <158290540+Dan-San837@users.noreply.github.com>
2025-02-03 01:51:24 -0500

    Delete INdiviadual1.1 directory

--- INdiviadual1.1/customers/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Cliente
-
-@admin.register(Cliente)
-class ClienteAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'email', 'telefono')
-    search_fields = ('nombre', 'email')
